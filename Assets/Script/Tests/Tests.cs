cc0865fbdea936f93fb6158075ad0e49cd618eb9
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-12 21:44:28 +0200

    Test: added Franc comparison to TestEquality

--- Assets/Script/Tests/Tests.cs
+++ Assets/Script/Tests/Tests.cs
@@ -19,6 +19,8 @@ public class Tests
     {
         Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
         Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+        Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
+        Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
     }
 
     [Test]
