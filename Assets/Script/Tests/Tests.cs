799b072d9dd97870be7fe4f5ea60b5bd7532ca66
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-12 21:50:35 +0200

    Test: compare Dollar and Franc case

--- Assets/Script/Tests/Tests.cs
+++ Assets/Script/Tests/Tests.cs
@@ -21,6 +21,7 @@ public class Tests
         Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
         Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
         Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
+        Assert.IsFalse(new Dollar(5).Equals(new Franc(5)));
     }
 
     [Test]
