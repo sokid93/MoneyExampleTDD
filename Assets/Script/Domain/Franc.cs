6966c1b0708098542b2d52af85b3daee40ea58f3
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-12 21:46:38 +0200

    Refactor: moved Equals from Franc to Money (Equals method generalized)

--- Assets/Script/Domain/Franc.cs
+++ Assets/Script/Domain/Franc.cs
@@ -1,9 +1,7 @@
 using System;
 
-public class Franc
+public class Franc : Money
 {
-    private int amount;
-
     public Franc(int amount)
     {
         this.amount = amount;
@@ -12,12 +10,6 @@ public class Franc
     public Franc times(int multiplier)
     {
         return new Franc(amount * multiplier);
-    }
-
-    override public bool Equals(object obj)
-    {
-        Franc franc = (Franc) obj;
-        return amount == franc.amount;
     }
 }
 
