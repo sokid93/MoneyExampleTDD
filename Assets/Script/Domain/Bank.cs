1fdd2b0b781a6656439a2efb6df22b9cb686d235
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-26 14:28:43 +0200

    Feat: reduce returns Money if it receives Money

--- Assets/Script/Domain/Bank.cs
+++ Assets/Script/Domain/Bank.cs
@@ -2,6 +2,8 @@ public class Bank
 {
     public Money reduce(Expression source, string to)
     {
+        if (source.GetType() == typeof(Money))
+            return (Money)source;
         Sum sum = (Sum)source;
         return sum.reduce(to);
     }
