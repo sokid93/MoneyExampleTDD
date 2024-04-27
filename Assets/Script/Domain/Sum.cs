fe8c331fc9b21c6bb1f5f272dd9cca300a5cd1fe
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-27 11:29:20 +0200

    FIx: we need to reduce both arguments when we reduce in Sum (test green)

--- Assets/Script/Domain/Sum.cs
+++ Assets/Script/Domain/Sum.cs
@@ -11,7 +11,7 @@ public class Sum : Expression
 
     public Money reduce(Bank bank, string to)
     {
-        int amount = augend.amount + addend.amount;
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
     }
 }
